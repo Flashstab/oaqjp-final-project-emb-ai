92b96e2ca96816a3f619217be1c40c41975abeca
Flashstab <jbenzinger2@gmail.com>
2026-09-16 21:16:56 -0400

    Task 3 work

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,5 +6,23 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     body = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers = headers, json = body)
+    
+    dic = response.json()
+    
+    emotions = dic['emotionPredictions'][0]['emotion']
+    angerScore = emotion['anger']
+    disgustScore = emotion['disgust']
+    fearScore = emotion['fear']
+    joyScore = emotion['joy']
+    sadnessScore = emotion['sadness']
+    
+    topScore = max(emotions, key=emotions.get())
 
-    return response.text
+    return {
+'anger': anger_score,
+'disgust': disgust_score,
+'fear': fear_score,
+'joy': joy_score,
+'sadness': sadness_score,
+'dominant_emotion': '<name of the dominant emotion>'
+}
