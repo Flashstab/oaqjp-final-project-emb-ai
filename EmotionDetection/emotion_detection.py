ac53d859c336ef917cd4327233c3669c64d3ccd1
Flashstab <jbenzinger2@gmail.com>
2026-09-21 20:41:06 -0400

    Catch up

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,9 @@ def emotion_detector(text_to_analyze):
     body = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers = headers, json = body)
     
+    if response.status_code == 400:
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+    
     dic = response.json()
     
     emotions = dic['emotionPredictions'][0]['emotion']
